99a978f8c2259a97cca1f3cbea7dc7afae02ca03
Hilal Merve Hancıoğlu <62815301+hilalmerve@users.noreply.github.com>
2021-01-03 23:32:53 +0300

    ilk gonderim

---
+++ kullaniciekle.sql
@@ -0,0 +1,28 @@
+﻿INSERT dbo.AspNetUsers
+(
+	Id,
+    Name,
+    SurName,
+    Password,
+    LockoutEndDateUtc,
+    AccessFailedCount,
+	EmailConfirmed,
+    PhoneNumberConfirmed,
+    TwoFactorEnabled,
+	LockoutEnabled,
+    UserName
+)
+
+VALUES
+(   N'1',
+    N'celal',       -- Name - nvarchar(max)
+    N'demir',       -- SurName - nvarchar(max)
+    N'123',       -- Password - nvarchar(max)
+    GETDATE(), -- LockoutEndDateUtc - datetime
+    0,         -- AccessFailedCount - int
+	0,
+	0,
+	0,
+	0,
+    N'celal258'        -- UserName - nvarchar(256)
+    )
